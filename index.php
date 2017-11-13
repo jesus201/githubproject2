b69a9caaa952daa8e6602fdea2119f743951d5a7
kennedy <iffkennedy@gmail.com>
2017-11-13 01:12:35 +0100

    diplay button

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
  <p>This is my second porject on heroku and github</p>
  <button>Click me</button>
  <a href="#">this is a link</a>
+  <button type="submit">Display submit buttons to me</button>
 </body>
 </html>
